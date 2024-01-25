325e5d18cab718f93cf98e4681cbcdf91556fd80
LucaB97 <luca.bertone97@gmail.com>
2024-01-25 16:04:29 +0100

    some adjustments

--- OpenMPI/hypo1run.sh
+++ OpenMPI/hypo1run.sh
@@ -6,7 +6,7 @@ CPUS=$2
 OPERATION=$3
 ###REPETITIONS=$4
 ALGS=("${@:4}")
-REPETITIONS=(1 10 100 1000 10000 100000 1000000)
+ITERATIONS=(1 10 100 1000 10000 100000 1000000)
 
 ##create array of values for number of processes (np) for mpirun
 k=2
@@ -39,11 +39,10 @@ do
 
         # Running some repetitions for each parameter configuration
         ###for X in $(seq 1 $REPETITIONS)
-        for X in "${REPETITIONS[@]}"
+        for X in "${ITERATIONS[@]}"
         do
             printf "\nIter: $X" >> "${OPERATION}_all_results_${NODE}.txt"
             printf "\nIter: $X" >> "tmp_results_${NODE}.txt"
-            salloc --nodelist=epyc005 --ntasks-per-node $CPUS --time=0:10:00
             mpirun -np $P --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_${OPERATION}_algorithm $A osu_${OPERATION} -f -z -i $X > "curr_results_${NODE}.txt"
             ###cat "curr_results_${NODE}.txt" >> "tmp_results_${NODE}.txt"
             cat "curr_results_${NODE}.txt" >> "${OPERATION}_all_results_${NODE}.txt"
@@ -58,7 +57,7 @@ printf "\n-------------------------------------------\n" >> "${OPERATION}_all_re
 rm curr_results_${NODE}.txt
 ###rm tmp_results_${NODE}.txt
 X=$(date +%m-%d--%H-%M)
-mv ${OPERATION}_all_results_${NODE}.txt ../results/hypothesis1/$OPERATION/$NODE/$X-all.txt
+mv ${OPERATION}_all_results_${NODE}.txt ../results/hypo1/$OPERATION/$NODE/$X-all.txt
 ###mkdir ../results/$OPERATION/$NODE/$X
 ###mv ${OPERATION}_all_results_${NODE}.txt ../results/$OPERATION/$NODE/$X/all.txt
 ###mv ${OPERATION}_summarized_results_${NODE}.csv ../results/$OPERATION/$NODE/$X/summarized.csv
