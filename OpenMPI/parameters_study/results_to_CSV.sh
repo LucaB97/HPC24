6c26e55f83e7fbfc644c39e5ae507f2d7afefc13
LucaB97 <luca.bertone97@gmail.com>
2024-08-09 16:59:13 +0200

    csv function

---
+++ OpenMPI/parameters_study/results_to_CSV.sh
@@ -0,0 +1,19 @@
+#usr/bin/bash
+
+folder=$1
+repetitions=10
+
+c++ createCSV.cpp -o createCSV
+mv createCSV iterations/tests/$folder
+cd iterations/tests/$folder
+
+printf "Operation,Algorithm,Mapping,Size,Avg Latency(us),Min Latency(us),Max Latency(us),Iterations\n" > "results.csv"
+
+for X in $(seq 1 $repetitions)
+    do
+        ./createCSV "test__$X.txt" "test__$X.csv" 
+        cat "test__$X.csv" >> "results.csv"
+        rm "test__$X.csv"
+    done
+
+rm createCSV
