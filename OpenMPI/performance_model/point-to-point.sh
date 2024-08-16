7e9c3a414910b75a7cc02501ecb81c574f2a2a77
LucaB97 <luca.bertone97@gmail.com>
2024-08-16 12:39:36 +0200

    results_performance

--- OpenMPI/performance_model/point-to-point.sh
+++ OpenMPI/performance_model/point-to-point.sh
@@ -2,22 +2,27 @@
 CPUS=$1
 MESSAGE_SIZE=$2
 
-cd ../operations/
-
 ##create array with IDs of receivers (sender is 0)
 k=1
 i=0
-while [[ $k < $CPUS ]] ; do
+while [[ $k -le $((CPUS-1)) ]] ; do
 receivers[$i]=$k
 k=$((k+1))
 i=$((i+1))
 done
 
+cd ../operations/
+module purge
+module load openMPI/4.1.6/gnu
+
+printf "" > "latencies_$MESSAGE_SIZE.txt"
+
 for R in "${receivers[@]}"
 do
-    printf "Message to core: $R\n" >> "latencies.txt"
+    printf "Message to core: $R\n"
+    printf "Message to core: $R\n" >> "latencies_$MESSAGE_SIZE.txt"
     mpirun -np 2 --cpu-list 0,$R osu_latency -m $MESSAGE_SIZE:$MESSAGE_SIZE > "temp.txt"
-    cat "temp.txt" >> "latencies.txt"
+    cat "temp.txt" >> "latencies_$MESSAGE_SIZE.txt"
 done
 
 rm temp.txt
