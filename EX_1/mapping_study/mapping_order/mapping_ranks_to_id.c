62442f950e02fc358617fb227423b0cb8354acec
LucaB97 <luca.bertone97@gmail.com>
2024-08-18 10:06:23 +0200

    bugs

--- EX_1/mapping_study/mapping_order/mapping_ranks_to_id.c
+++ EX_1/mapping_study/mapping_order/mapping_ranks_to_id.c
@@ -1,62 +1,70 @@
+#define _GNU_SOURCE
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
 
 int main(int argc, char *argv[]) {
     int rank, size, len;
     char hostname[MPI_MAX_PROCESSOR_NAME];
-    
+
     // Initialize the MPI environment
     MPI_Init(&argc, &argv);
-    
+
     // Get the number of processes
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    
+
     // Get the rank of the process
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     // Get the name of the processor
     MPI_Get_processor_name(hostname, &len);
-    
+
     // Get the CPU ID where the process is currently running
-    int core_id = sched_getcpu();
-    
-    // Allocate an array to store core_ids of all ranks (only on the root process)
+    unsigned int cpu_id, node_id;
+    if (getcpu(&cpu_id, &node_id) == -1) {
+        perror("getcpu");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    int core_id = (int)cpu_id;
+
+    // Print the core ID for each rank (debug)
+    printf("Rank %d on host %s is on core %d\n", rank, hostname, core_id);
+
+    // Root process collects all core IDs
     int *core_ids = NULL;
     if (rank == 0) {
-        core_ids = malloc(size * sizeof(int));
+        core_ids = (int *)malloc(size * sizeof(int));
+        if (core_ids == NULL) {
+            perror("malloc");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
     }
-    
-    // Gather all core_ids to the root process (rank 0)
+
+    // Gather all core IDs to the root process
     MPI_Gather(&core_id, 1, MPI_INT, core_ids, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Only the root process saves the data to a CSV file
+    // Root process writes the core IDs to a CSV file
     if (rank == 0) {
-        char filename[256];
-        if (argc > 1) {
-            snprintf(filename, sizeof(filename), "%s.csv", argv[1]);
-        } else {
-            snprintf(filename, sizeof(filename), "core_mapping.csv");
+        FILE *file = fopen(argv[1], "w");
+        if (file == NULL) {
+            perror("fopen");
+            MPI_Abort(MPI_COMM_WORLD, 1);
         }
 
-        FILE *fp = fopen(filename, "w");
-        if (fp == NULL) {
-            perror("Error opening file");
-            exit(EXIT_FAILURE);
-        }
-
-        fprintf(fp, "Rank,Core_ID\n");
+        fprintf(file, "Rank,Core_ID\n");
         for (int i = 0; i < size; i++) {
-            fprintf(fp, "%d,%d\n", i, core_ids[i]);
+            fprintf(file, "%d,%d\n", i, core_ids[i]);
         }
 
-        fclose(fp);
+        fclose(file);
         free(core_ids);
     }
-    
+
     // Finalize the MPI environment
     MPI_Finalize();
-    
+
     return 0;
 }
