2aa9a1e74b23837ad1aa36d35d14d4e829cff39e
LucaB97 <luca.bertone97@gmail.com>
2024-08-25 19:21:26 +0200

    .

--- EX_1/mapping_study/tests/mapping_variation.sh
+++ EX_1/mapping_study/tests/mapping_variation.sh
@@ -1,41 +1,30 @@
 #/usr/bin/bash
 
-X=$(date +%m-%d--%H-%M)
-mkdir $X
-
 ##get info for the execution
 node=$1
 cpus=$2
-# repetitions=$3
+operation=$3
+algorithms=("${@:4}")
 
-## chosen after previous test on total iterations and warmup iterations 
-ITERATIONS=10000
-WARMUP=200
-
-## these parameters will be generated randomly at each repetition:
-## the idea is to find out if it is possible to study the impact of **total iterations** and **warmup iterations**
-## on performances independently on the specific execution 
-OPERATION="bcast"
-ALGS=(0 2 5 6 7)
+mkdir $node/
+mkdir $node/$operation
 
 ## ITERATION ON THESE VALUES
-MAPPING=("core" "L3cache" "numa" "socket" "node")
+MAPPINGS=("core" "L3cache" "numa" "socket" "node")
 
 cd ../../OPERATIONS/
 module purge
 module load openMPI/4.1.6/gnu
 
-
 # For-loop on the used algorithms
-for A in "${ALGS[@]}"
+for A in "${algorithms[@]}"
 do
-    for M in "${MAPPING[@]}"
+    for M in "${MAPPINGS[@]}"
     do
-        printf "\nMapping: $M" >> "bcast__${A}.txt"
-        mpirun -np $cpus --map-by $M --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_${OPERATION}_algorithm $A osu_${OPERATION} -f -z > "curr_results.txt"
-        cat "curr_results.txt" >> "bcast__${A}.txt"
+        printf "\nMapping: $M" >> "${operation}__alg__${A}.txt"
+        mpirun -np $cpus --map-by $M --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_${operation}_algorithm $A osu_${operation} -f -z > "curr__${operation}__${A}.txt"
+        cat "curr__${operation}__${A}.txt" >> "${operation}__alg__${A}.txt"
     done
-    mv "bcast__${A}.txt" ../mapping_study/tests/$X/
+    rm curr__${operation}__${A}.txt
+    mv "${operation}__alg__${A}.txt" ../mapping_study/tests/$node/$operation/alg__${A}.txt
 done
-
-rm curr_results.txt
