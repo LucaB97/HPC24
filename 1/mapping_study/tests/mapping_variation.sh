787bbf7d9553bcb2a6277e854a0916fc14e1012c
LucaB97 <luca.bertone97@gmail.com>
2024-09-23 09:30:18 +0200

    cleaned mapping folder

--- 1/mapping_study/tests/mapping_variation.sh
+++ 1/mapping_study/tests/mapping_variation.sh
@@ -10,7 +10,7 @@ mkdir $node/
 mkdir $node/$operation
 
 ## ITERATION ON THESE VALUES
-MAPPINGS=("core" "L3cache" "numa" "socket" "node")
+MAPPINGS=("core" "numa" "node")
 
 cd ../../OPERATIONS/
 module purge
@@ -22,9 +22,10 @@ do
     for M in "${MAPPINGS[@]}"
     do
         printf "\nMapping: $M" >> "${operation}__alg__${A}.txt"
-        mpirun -np $cpus --map-by $M --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_${operation}_algorithm $A osu_${operation} -f -z > "curr__${operation}__${A}.txt"
-        cat "curr__${operation}__${A}.txt" >> "${operation}__alg__${A}.txt"
+        mpirun -np $cpus --map-by $M --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_${operation}_algorithm $A osu_${operation} -f -z >> "${operation}__alg__${A}.txt"
     done
-    rm curr__${operation}__${A}.txt
+    
     mv "${operation}__alg__${A}.txt" ../mapping_study/tests/$node/$operation/alg__${A}.txt
 done
+
+bash results_to_plots.sh $node $operation $algorithms
