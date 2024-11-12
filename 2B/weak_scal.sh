adf6fe8853e29fb3b349ea11c7184140f130e1f2
LucaB97 <luca.bertone97@gmail.com>
2024-11-12 11:11:51 +0100

    tidied environment 2B

--- 2B/weak_scal.sh
+++ 2B/weak_scal.sh
@@ -9,18 +9,19 @@ srun --nodes=$NODES --ntasks-per-node=1 free > "memory"
 free_memory=$(grep Mem memory | awk '{print $4}' | sort -n | head -n 1)  #in kibibytes (KiB), where 1 KiB = 1024 bytes
 gcc -o memory_req memory_req.c
 data_size=$(./memory_req)  # Size of each data entry in bytes
-max_segment_size=$(echo "scale=0; 0.25 * 1024 * $free_memory / (3 * $data_size * ${NUMPROCS[-1]})" | bc)
+max_segment_size=$(echo "scale=0; 0.7 * 1024 * $free_memory / (7.5 * $data_size * 128)" | bc)
+#max_segment_size=$(echo "scale=0; 0.7 * 1024 * $free_memory / (7.5 * $data_size * ${NUMPROCS[-1]})" | bc)
 printf "max segment size: $max_segment_size\n"
 
-##if [ $SIZE -ge $max_segment_size ]; then
-##    SIZE=$max_segment_size
-##fi
+#SIZE=$max_segment_size
+#if [ $SIZE -ge $max_segment_size ]; then
+#    SIZE=$max_segment_size
+#fi
 
 ##load modules & create executable for the algorithm
 module purge
 module load openMPI/4.1.6/gnu
-# mpicc -O3 hybrid_qsort.c -o qsort
-mpicc -O3 new_qsort.c -o qsort
+mpicc -O3 qsort.c -o qsort
 
 ##perform strong scalability test
 printf "Test\tProcesses\tSize\t\tTotal\t\tCommunication\tSorting\t\tMerging\n" > "Weak.txt"
